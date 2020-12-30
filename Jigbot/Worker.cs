80ba0329fad5695332391bbf7b6194ce4253b34f
Eric Esquibel <jignate@outlook.com>
2020-12-29 23:58:15 -0700

    Handle asynchronous parsing of embeds

--- Jigbot/Worker.cs
+++ Jigbot/Worker.cs
@@ -29,6 +29,7 @@ namespace Jigbot
         private readonly WebClient WebClient;
         private readonly Emoji Check;
         private readonly SHA1CryptoServiceProvider SHA1;
+        private readonly Regex HasUrl;
         private Dictionary<ulong, RestUserMessage> History;
         private string[] Data;
 
@@ -48,6 +49,7 @@ namespace Jigbot
                     WebClient = new WebClient();
                     Check = new Emoji("\u2611\uFE0F");
                     SHA1 = new SHA1CryptoServiceProvider();
+                    HasUrl = new Regex(@"https?://[\w\d\-\.\/]+\.(jpg|jpeg|gif|png)", RegexOptions.IgnoreCase);
                 } else
                 {
                     logger.LogError("UPLOADS directory does not exist: {uplodas}", new { uploads });
@@ -58,12 +60,59 @@ namespace Jigbot
             History = new Dictionary<ulong, RestUserMessage>();
             CommandX = new Regex(@"\!" + Regex.Escape(Command) + @"[\s]*(.*)", RegexOptions.IgnoreCase);
 
-            discord = new DiscordSocketClient();
+            discord = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                // Increases memory usage, but needed to get embeds that are parsed asynchonously and sent via MESSAGE_UPDATE event
+                MessageCacheSize = 128,
+            });
+
             discord.Log += Discord_Log;
             discord.Ready += Discord_Ready;
             discord.MessageReceived += Discord_MessageReceived;
+            discord.MessageUpdated += Discord_MessageUpdated;
         }
 
+        private async Task Discord_MessageUpdated(Cacheable<IMessage, ulong> cacheableBefore, SocketMessage after, ISocketMessageChannel channel)
+        {
+            // Upload directory not set, nothing to do
+            if (string.IsNullOrEmpty(Uploads))
+            {
+                return;
+            }
+
+            // If original message not in cache, too old and we don't care about it
+            if (!cacheableBefore.HasValue)
+            {
+                return;
+            }
+
+            // No embeds
+            if (after.Embeds.Count == 0)
+            {
+                return;
+            }
+
+            var before = cacheableBefore.Value;
+
+            // Is a message we care about?
+            if (CommandX.IsMatch(after.Content ?? before.Content))
+            {
+                // Are there new embeds able?
+                if (after.Embeds.Count <= before.Embeds.Count)
+                {
+                    return;
+                }
+
+                // Have we processed this message before?
+                if (before.Reactions.Values.Any(reaction => reaction.IsMe))
+                {
+                    return;
+                }
+                
+                await DownloadEmbeds(after);
+            }
+        }
+        
         private async Task Discord_MessageReceived(SocketMessage message)
         {
             // The bot should never respond to itself.
@@ -139,42 +188,12 @@ namespace Jigbot
                         return;
                     }
 
-                    byte i = 0;
-                    var react = false;
-                    var tasks = new Task[message.Embeds.Count];
-
-                    foreach (var embed in message.Embeds)
+                    await DownloadEmbeds(message);
+                } else
+                {
+                    if (HasUrl.IsMatch(message.Content))
                     {
-                        var uri = new Uri(embed.Url);
-                        var info = new FileInfo(uri.LocalPath);
-
-                        switch(info.Extension.ToLower())
-                        {
-                            case ".jpg":
-                            case ".jpeg":
-                            case ".gif":
-                            case ".png":
-                                tasks[i++] = Download(uri);
-                                react = true;
-                                break;
-                            default:
-                                tasks[i++] = Task.FromResult<object>(null);
-                                break;
-                        }
-                    }
-
-                    Task.WaitAll(tasks);
-
-                    try
-                    {
-                        if (react)
-                        {
-                            await message.AddReactionAsync(Check);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        logger.LogError(e, e.Message);
+                        return;
                     }
                 }
                 
@@ -207,6 +226,47 @@ namespace Jigbot
                 }
 
                 return;
+            }
+        }
+
+        private async Task DownloadEmbeds(IMessage message)
+        {
+            byte i = 0;
+            var react = false;
+            var tasks = new Task[message.Embeds.Count];
+
+            foreach (var embed in message.Embeds)
+            {
+                var uri = new Uri(embed.Url);
+                var info = new FileInfo(uri.LocalPath);
+
+                switch (info.Extension.ToLower())
+                {
+                    case ".jpg":
+                    case ".jpeg":
+                    case ".gif":
+                    case ".png":
+                        tasks[i++] = Download(uri);
+                        react = true;
+                        break;
+                    default:
+                        tasks[i++] = Task.FromResult<object>(null);
+                        break;
+                }
+            }
+
+            Task.WaitAll(tasks);
+
+            try
+            {
+                if (react)
+                {
+                    await message.AddReactionAsync(Check);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, e.Message);
             }
         }
 
